a6d5b81853c9dc4157d82e136ad2b27532ad9b7a
Assa <codermasochist@gmail.com>
2025-01-14 19:35:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 window.addEventListener('scroll', function () {
-    // Находим элемент с классом .video-background
     let video = document.querySelector('.video-background');
-
-    // Получаем текущую прокрутку страницы
     let scrollPosition = window.scrollY;
-
-    // Применяем размытие в зависимости от прокрутки
-    video.style.filter = `blur(${scrollPosition / 10}px)`; // Увеличиваем размытие с прокруткой
+    video.style.filter = `blur(${scrollPosition / 10}px)`;
 });
 
-// Убираем размытие после загрузки видео
 let video = document.querySelector('.video-background');
 video.addEventListener('loadeddata', function() {
-    video.classList.add('loaded'); // Убираем размытие
+    video.classList.add('loaded');
 });
